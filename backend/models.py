6788895092ca459984f195cafd2c7a0ab65fbe22
nizquier <97483675+nizquier@users.noreply.github.com>
2023-10-18 18:03:18 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -25,4 +25,17 @@ class Lobby(db.Model):
     game_id = db.Comuln(db.Integer)
     host_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     num_players = db.Comuln(db.Integer)
+    description = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Lobby_Players(db.Model):
+    player_1 = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    player_2 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_3 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_4 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_5 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_6 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_7 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_8 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_9 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
+    player_10 = db.Column(db.Integer, db.ForeignKey('user.user_id'), default=None)
