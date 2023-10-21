5326b18de4bf4a153697fa901a7020877b724975
Justin Privett <Justin.Privett1994@gmail.com>
2023-10-21 13:48:28 -0400

    added frontend password validation

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -10,16 +10,32 @@ export class RegistrationComponent {
   user: any = {
     username: '',
     email: '',
-    password1: '',
-    password2: ''
+    password: '',
+    confirmPassword: ''
   };
+  passwordsMatch = true;
+  passwordLength = true;
 
   constructor(private http:HttpClient){}
+
   onSubmit() {
+    this.passwordsMatch = true;
+    this.passwordLength = true;
+
+    if (this.user.password !== this.user.confirmPassword) {
+      console.error('Passwords do not match');
+      this.passwordsMatch = false;
+      return;
+    }
+
+    if (this.user.password.length < 8 || this.user.password.length > 64) {
+      this.passwordLength = false;
+      return;
+    }
+
     this.http.post('http://localhost:5000/register', this.user)
     .subscribe((result)=>{
       console.warn(result);
     });
-
   }
 }
