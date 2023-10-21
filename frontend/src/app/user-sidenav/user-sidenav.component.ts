2a979d14af5fcd3293d1cd3f4fab088fde81bf36
Miguel <IronsideGames@outlook.com>
2023-10-21 02:29:08 -0400

    Side nav bar added

---
+++ frontend/src/app/user-sidenav/user-sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-sidenav',
+  templateUrl: './user-sidenav.component.html',
+  styleUrls: ['./user-sidenav.component.css']
+})
+export class UserSidenavComponent {
+
+}
