90dee02a5533ee79738d09e74d567b13046df1c7
Miguel <IronsideGames@outlook.com>
2023-10-25 16:16:50 -0400

    Added new side nav bar

--- frontend/src/app/user-sidenav/user-sidenav.component.ts
+++ frontend/src/app/user-sidenav/user-sidenav.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-sidenav.component.css']
 })
 export class UserSidenavComponent {
-  
+  sidebarClosed: boolean = false;
+  activeSubMenu: string = '';
+
+  toggleSubMenu(subMenu: string) {
+    this.activeSubMenu = this.activeSubMenu === subMenu ? '' : subMenu;
+  }
+
+  toggleSidebar() {
+    this.sidebarClosed = !this.sidebarClosed;
+  }
+
+  isSubMenuActive(subMenu: string): boolean {
+    return this.activeSubMenu === subMenu;
+  }
 }
