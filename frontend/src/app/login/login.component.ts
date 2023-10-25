61af83076acf8f7d927a2f1361c2b6112b50b691
Justin Privett <Justin.Privett1994@gmail.com>
2023-10-24 22:16:16 -0400

    implemented backend and frontend for json web tokens

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,24 @@ export class LoginComponent {
     password: ''
   };
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private jwtHelper: JwtHelperService){}
   onSubmit() {
-    this.http.post('http://localhost:5000/login', this.user)
-    .subscribe((result)=>{
-      console.warn(result);
-    });
+    this.http.post('http://localhost:5000/api/login', this.user)
+    .subscribe((result: any)=>{
+      if (result && result.access_token) {
+        localStorage.setItem('access_token', result.access_token)
 
+        //testing. delete later
+        console.log(this.jwtHelper.decodeToken(result.access_token.username))
+
+        if (!this.jwtHelper.isTokenExpired(result.access_token)) {
+          console.log('Login successful');
+        } else {
+          console.error('Token is expired');
+        }
+      } else {
+        console.error('Login failed');
+      }      
+    });
   }
 }
--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-registration',
@@ -16,26 +17,44 @@ export class RegistrationComponent {
   passwordsMatch = true;
   passwordLength = true;
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private jwtHelper: JwtHelperService){}
 
   onSubmit() {
+    // Reset validation flags
     this.passwordsMatch = true;
     this.passwordLength = true;
 
+    // Check if passwords match
     if (this.user.password !== this.user.confirmPassword) {
       console.error('Passwords do not match');
       this.passwordsMatch = false;
       return;
     }
-
+    
+    // Check password length
     if (this.user.password.length < 8 || this.user.password.length > 64) {
+      console.error('Password must be at least 8 characters');
       this.passwordLength = false;
       return;
     }
 
-    this.http.post('http://localhost:5000/register', this.user)
-    .subscribe((result)=>{
-      console.warn(result);
+    // Registration request
+    this.http.post('http://localhost:5000/api/register', this.user)
+    .subscribe((result: any)=>{
+      if (result && result.access_token) {
+        localStorage.setItem('access_token', result.access_token)
+
+        //testing. delete later
+        console.log(this.jwtHelper.decodeToken(result.access_token.username))
+
+        if (!this.jwtHelper.isTokenExpired(result.access_token)) {
+          console.log('Registration and login successful');
+        } else {
+          console.error('Token is expired');
+        }
+      } else {
+        console.error('Registration failed');
+      }      
     });
   }
 }
