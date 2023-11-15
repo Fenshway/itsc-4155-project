36d5e40765db10b8ef876ef4b5b0ae31602ea4ce
Justin Privett <Justin.Privett1994@gmail.com>
2023-11-14 22:10:36 -0500

    added route guards. will update later but good for now

--- frontend/src/app/is-authenticated.guard.ts
+++ frontend/src/app/is-authenticated.guard.ts
@@ -1,5 +1,12 @@
 import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  return true;
+  const token = localStorage.getItem('access_token');
+  if (!token) {
+    const router = inject(Router)
+    return router.createUrlTree(['/login']);
+  }
+  return true
 };
